60f6ee76e2b626ad51059f5a61e1306229922b18
Jason Stout <30294372+HangoverSamui@users.noreply.github.com>
2021-06-26 19:12:28 -0500

    Initial Commit

---
+++ Notes WatchKit Extension/NotesApp.swift
@@ -0,0 +1,24 @@
+//
+// Swift Version 5.0
+// macOS Version 11.4
+//  NotesApp.swift
+//  Notes WatchKit Extension
+//
+//  Created by Jason Stout on 6/26/21 . 
+//  
+//  Design is not just what it looks like and feels like. Design is how it works. - Steve Jobs
+//  
+
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
